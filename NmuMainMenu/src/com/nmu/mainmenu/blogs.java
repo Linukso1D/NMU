b9ebc1fd0c045941a092fd61dfe54794239a810e
Максим <@NOUTMAX>
2015-01-22 16:24:25 +0200

    final freecmp and blogs

--- NmuMainMenu/src/com/nmu/mainmenu/blogs.java
+++ NmuMainMenu/src/com/nmu/mainmenu/blogs.java
@@ -1,12 +1,68 @@
 package com.nmu.mainmenu;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class blogs extends Activity  {
+public class blogs extends Activity implements OnClickListener {
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.blogs);
+
+		ImageView btn_twitter = (ImageView) findViewById(R.id.btn_twitter);
+		btn_twitter.setOnClickListener(this);
+		btn_twitter.setClickable(true); 
+
+		ImageView btn_vk = (ImageView) findViewById(R.id.btn_vk);
+		btn_vk.setOnClickListener(this);
+		btn_vk.setClickable(true); 
+
+		ImageView btn_youtube = (ImageView) findViewById(R.id.btn_youtube);
+		btn_youtube.setOnClickListener(this);
+		btn_youtube.setClickable(true); 
+
+		ImageView btn_facebook = (ImageView) findViewById(R.id.btn_facebook);
+		btn_facebook.setOnClickListener(this);
+		btn_facebook.setClickable(true); 
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.btn_twitter:
+			Intent browserIntent_twitter = new Intent(Intent.ACTION_VIEW,
+					Uri.parse("https://twitter.com/dvnz_nmu"));
+			startActivity(browserIntent_twitter);
+			break;
+
+		case R.id.btn_vk:
+			Intent browserIntent_vk = new Intent(Intent.ACTION_VIEW,
+					Uri.parse("https://vk.com/public28412208"));
+			startActivity(browserIntent_vk);
+			break;
+
+		case R.id.btn_facebook:
+			Intent browserIntent_facebook = new Intent(Intent.ACTION_VIEW,
+					Uri.parse("https://www.facebook.com/pages/National-Mining-University-NMU/155897531176446"));
+			startActivity(browserIntent_facebook);
+			break;
+
+		case R.id.btn_youtube:
+			Intent browserIntent_youtube = new Intent(Intent.ACTION_VIEW,
+					Uri.parse("https://www.youtube.com/user/NMUchannel"));
+			startActivity(browserIntent_youtube);
+			break;
+		}
+
 	}
 
 }
+
--- NmuMainMenu/src/com/nmu/mainmenu/freepc.java
+++ NmuMainMenu/src/com/nmu/mainmenu/freepc.java
@@ -3,53 +3,69 @@ package com.nmu.mainmenu;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class freepc extends Activity {
+	TextView tv;
+	WebView browser;
+	Document doc;
+	int pos;
 
-	
+	@SuppressLint("JavascriptInterface")
 	protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	TextView tv = (TextView) findViewById(R.id.textView1);
-	setContentView(R.layout.freepc);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.freepc);
+		tv = (TextView) findViewById(R.id.tv);
+		browser = (WebView) findViewById(R.id.wv);
+	
+		class MyJavaScriptInterface {
+			@SuppressWarnings("unused")
+			public void processHTML(String html) {
+				doc = Jsoup.parse(html);
+				StringBuffer sb = new StringBuffer(doc.text());
+				pos = sb.lastIndexOf("В преподавательской");
+				sb = sb.delete(0, pos);
+				pos = sb.lastIndexOf("Автор");
+				sb = sb.delete(pos, sb.length());
+				String res = sb.toString();
+				res = res.replaceAll("В аудитории", "\nВ аудитории");
+				new SetText().execute(res);
+				Log.i("Logs", res);
+			}
+		}
+		/* JavaScript must be enabled if you want it to work, obviously */
+		browser.getSettings().setJavaScriptEnabled(true);
+		/* Register a new JavaScript interface called HTMLOUT */
+		browser.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+		/* WebViewClient must be set BEFORE calling loadUrl! */
+		browser.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				browser.loadUrl("javascript:window.HTMLOUT.processHTML('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+			}
+		});
+		browser.loadUrl("http://m.nmu.org.ua/#freecomp");
+			}
+	public class SetText extends AsyncTask<String, Void, String> {
 
-	String html = "<html><head><title>Коты учатся кодить</title>"
-			+ "<body><p>Коты умеют <del>ш</del>кодить.<br> Они великие программисты." +
-					"<p>А еще они умеют мяукать.</p>" +
-					"<a href='http://developer.alexanderklimov.ru'>Подробности здесь</a>" +
-					"</body></html>";
-		
-		Document doc = Jsoup.parse(html);
-		//doc.html()
-		tv.setText("lol"); 
-	
-	
-	//Document doc = Jsoup.parse("http://m.nmu.org.ua/#freecomp");
-	//tv_1.setText(doc.html());
-	//Element loginform = doc.getElementById("freecomp");
-	
+		@Override
+		protected String doInBackground(String... links) {
+			
+			return links[0];
+		}
 
+		@Override
+		protected void onPostExecute(String result) {
+			tv.setText(result);
+		}
 
-//	Element content = doc.getElementById("freecomp");//<- вот на этой строчке вылетает
-	//Elements links = content.getElementsByTag("br");
-	
-	/*
-	 я сначала пробовал загрузить страницу. не получилось. потом я попробовал использовать уже 
-	 сохраненную стр.
-	 try {
-		doc  = Jsoup.connect("http://m.nmu.org.ua/#freecomp").get();
-		
-	} catch (IOException e) {
-  		e.printStackTrace();
-	}
-	//String title = doc.title();
-	//tv_1.setText(title);*/
-	
-	/*Element Element = doc.select("br").first();
-	String linkHref = Element.attr("href"); 
-	tv_1.setText(linkHref); */
 	}
 
 }
